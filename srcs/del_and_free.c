422ecda1c17524a6807a3ecb7d9abbfc6b802bf4
Ahmad Alwi Syed <asyed@c1r8s4.42singapore.sg>
2024-06-07 11:32:33 +0800

    working and free of memory leak, added free guarding

--- srcs/del_and_free.c
+++ srcs/del_and_free.c
@@ -6,7 +6,7 @@
 /*   By: asyed <asyed@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 14:39:54 by asyed             #+#    #+#             */
-/*   Updated: 2024/06/07 10:37:29 by asyed            ###   ########.fr       */
+/*   Updated: 2024/06/07 11:30:52 by asyed            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,13 @@ void	free_split(char **args)
 {
 	int	i;
 
+	if (args == NULL)
+		return ;
 	i = 0;
 	while (args[i])
 	{
 		free(args[i]);
+		args[i] = NULL;
 		i++;
 	}
 	free(args);
@@ -29,22 +32,37 @@ void	free_env(t_env	**env_list)
 	t_env	*tmp;
 	t_env	*next;
 
+	if (env_list == NULL || *env_list == NULL)
+		return ;
 	tmp = *env_list;
 	while (tmp)
 	{
 		next = tmp->next;
-		free(tmp->key);
-		free(tmp->value);
+		if (tmp->key)
+		{
+			free(tmp->key);
+			tmp->key = NULL;
+		}
+		if (tmp->value)
+		{
+			free(tmp->value);
+			tmp->value = NULL;
+		}
 		free(tmp);
 		tmp = next;
 	}
-	free(env_list);
+	*env_list = NULL;
+	//free(env_list);
 }
 
 void	free_all(char **args, char *input, t_env **env_dup)
 
 {
 	free_split(args);
-	free(input);
+	if (input)
+	{
+		free(input);
+		input = NULL;
+	}
 	free_env(env_dup);
 }
