464078e7d6980e240e0ebb3b69d8b4d3d3995304
Ahmad Alwi Syed <asyed@c1r8s4.42singapore.sg>
2024-06-11 15:32:27 +0800

    freeing merged

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: asyed <asyed@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 13:58:54 by ting              #+#    #+#             */
-/*   Updated: 2024/06/11 15:18:36 by asyed            ###   ########.fr       */
+/*   Updated: 2024/06/11 15:32:03 by asyed            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,15 @@ int	main(int ac, char **av, char **env)
 
 	(void)ac;
 	(void)av;
-	cmds = (t_cmd **)malloc(sizeof(t_cmd *));
-	*cmds = NULL;
+
 	env_dup = init_env_copy(env);
 	if (env_dup == NULL)
 	{
 		perror ("init_env_copy");
 		return (1);
 	}
+	cmds = (t_cmd **)malloc(sizeof(t_cmd *));
+	*cmds = NULL;
 	while (1)
 	{
 		line = readline(C "shell@st42:$ " RST);
@@ -38,11 +39,6 @@ int	main(int ac, char **av, char **env)
 		{
 			lexer_and_parse(cmds, line);
 			free(line);
-			printf("After lexer:\n");
-			print_lexer(lexer);
-			parsing(lexer, cmds);
-			print_parse(cmds);
-			free_lexer(lexer);
 			current = *cmds;
 			while(current)
 			{
@@ -51,7 +47,6 @@ int	main(int ac, char **av, char **env)
 				current = current->next;
 			}
 			free_cmds(cmds);
-		//	freeall_and_exit(lexer, cmds ,line); //should be in exit func
 		}
 	}
 	return (0);
