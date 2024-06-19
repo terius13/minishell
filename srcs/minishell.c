958896c5bd7bc90ea10879f8862b7c73fac41fd3
Ahmad Alwi Syed <asyed@c1r8s10.42singapore.sg>
2024-06-19 19:19:12 +0800

    env PWD updated, signal next

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: asyed <asyed@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 13:58:54 by ting              #+#    #+#             */
-/*   Updated: 2024/06/19 19:11:05 by asyed            ###   ########.fr       */
+/*   Updated: 2024/06/19 19:18:33 by asyed            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	signal_handlers_setup()
 }
 */
 
-/*
+
 int	main(int ac, char **av, char **env)
 {
 	char		*line;
@@ -55,7 +55,7 @@ int	main(int ac, char **av, char **env)
 		if (line && *line)
 		{
 			add_history(line);
-			if (lexer_and_parse(cmds, line, env_dup))
+			if (lexer_and_parse(cmds, line, env_dup, status))
 			{
 				free_cmds(cmds);
 				free(line); //prob dont need to free, only in exit
@@ -73,64 +73,4 @@ int	main(int ac, char **av, char **env)
 		}
 	}
 	return (status->exit_status);
-}*/
-int	main(int ac, char **av, char **env)
-{
-	char		*line;
-	t_cmd		**cmds;
-	t_env		**env_dup;
-	t_cmd		*current;
-	t_ms_state	*update;
-
-	(void)ac;
-	(void)av;
-
-	// signal_handlers_setup();
-	update = (t_ms_state *)malloc(sizeof(t_ms_state));
-	if (update == NULL)
-	{
-		perror ("init_env_copy");
-		return (1);
-	}
-	update->exit_status = 0;
-	env_dup = init_env_copy(env);
-	if (env_dup == NULL)
-	{
-		perror ("init_env_copy");
-		free(update);
-		return (1);
-	}
-	cmds = (t_cmd **)malloc(sizeof(t_cmd *));
-	*cmds = NULL;
-	while (1)
-	{
-		line = readline(C "shell@st42:$ " RST);
-		if (line == NULL)
-		{
-			ft_putendl_fd("exit", STDOUT_FILENO); // Handle Ctrl + D
-			rl_clear_history();
-			break ; // exit if EOF or error, can be Ctrl + D
-		}
-		if (line && *line)
-		{
-			add_history(line);
-			if (lexer_and_parse(cmds, line, env_dup, update))
-			{
-				free_cmds(cmds);
-				free(line); //prob dont need to free, only in exit
-				continue;
-			}
-			free(line);
-			current = *cmds;
-			while(current)
-			{
-				if (current->builtin)
-					execute_builtins(cmds, current->cmd_arr, env_dup, update);
-				current = current->next;
-			}
-			free_cmds(cmds);
-		}
-	}
-	rl_clear_history();
-	return (update->exit_status);
 }
