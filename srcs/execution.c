18643f3edebca7d9b3daeeb74d322e317315c786
Timberly Ng <ting@c1r8s6.42singapore.sg>
2024-06-21 17:16:03 +0800

    fixing bug

--- srcs/execution.c
+++ srcs/execution.c
@@ -6,7 +6,7 @@
 /*   By: ting <ting@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 15:10:54 by ting              #+#    #+#             */
-/*   Updated: 2024/06/21 14:20:23 by ting             ###   ########.fr       */
+/*   Updated: 2024/06/21 17:15:20 by ting             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,6 @@
 
 // pipefd[0] is for reading end
 // pipefd[1] is for writing end
-// int	execution(t_cmd **cmds, t_env **env, t_ms_state *status)
-// {
-// 	t_cmd	*current;
-// 	int		pipefd[2];
-// 	int		pid;
-
-// 	current = *cmds;
-// 	while (current)
-// 	{
-// 		if (current->next)
-// 			pipe(pipefd);
-// 		pid = fork();
-// 		if (pid < 0)
-// 			perror("fork error");
-// 		if (pid == 0)
-// 		{
-// 			dup2(pipefd[0], STDIN_FILENO);
-// 			close(pipefd[0]);
-// 			dup2(pipefd[1], STDOUT_FILENO);
-// 			close(pipefd[1]);
-// 			handle_cmd(cmds, current, env, status);
-// 		}
-// 		current = current->next;
-// 	}
-// 	waitpid(pid, NULL, 0);
-// 	return (0);
-// }
-
-// Function to concatenate key and value into "key=value"
 
 void	execute_cmd(t_cmd *cmd, t_env **env, t_ms_state *status)
 {
@@ -93,63 +64,46 @@ void	do_single_cmd(t_cmd **cmds, t_env **env, t_ms_state *status)
 		status->exit_status = WEXITSTATUS(exit_status);
 }
 
-// int	cmds_len(t_cmd *cmds)
-// {
-// 	t_cmd	*current;
-// 	int 	num_cmds;
-
-// 	num_cmds = 0;
-// 	while (current)
-//     {
-//         num_cmds++;
-//         current = current->next;
-//     }
-// 	return (num_cmds);
-// }
-
-
-void	execute_pipeline(t_cmd **cmds, t_env **env, t_ms_state *status)
+int	cmds_len(t_cmd *cmds)
 {
-	int		num_cmds;
 	t_cmd	*current;
-	int		i;
-	int		pid;
-	int		exit_status;
-	int		**pipe_ends;
-	int		j;
-	//int	stat;
+	int 	num_cmds;
 
 	num_cmds = 0;
-	current = *cmds;
+	current = cmds;
 	while (current)
-	{
-		num_cmds++;
-		current = current->next;
-	}
+    {
+        num_cmds++;
+        current = current->next;
+    }
+	return (num_cmds);
+}
 
-	// Dynamically allocate memory for pipe_ends
+// Dynamically allocate memory for pipe_ends
+int	**init_pipe_ends(int num_cmds)
+{
+	int		**pipe_ends;
+	int		i;
+	
+	i = 0;
 	pipe_ends = (int **)malloc(sizeof(int *) * (num_cmds - 1));
 	if (!pipe_ends)
-	{
-		perror("malloc error");
-		return ;
-	}
-	i = 0;
+		return (perror("malloc error"), NULL);
 	while (i < num_cmds - 1)
 	{
 		pipe_ends[i] = (int *)malloc(sizeof(int) * 2);
 		if (!pipe_ends[i])
-		{
-			perror("malloc error");
-			// Free previously allocated memory in case of error
-			while (--i >= 0)
-				free(pipe_ends[i]);
-			free(pipe_ends);
-			return ;
-		}
+			return (perror("malloc error"), NULL);
 		i++;
 	}
+	return (pipe_ends);
+}
 
+void	init_pipes(int **pipe_ends, int num_cmds)
+{
+	int		i;
+	int		j;
+	
 	i = 0;
 	while (i < num_cmds - 1)
 	{
@@ -164,15 +118,56 @@ void	execute_pipeline(t_cmd **cmds, t_env **env, t_ms_state *status)
 				j++;
 			}
 			free(pipe_ends);
-			return ;
+			return  ;
 		}
 		i++;
 	}
+}
+
+void	execute_pipeline(t_cmd **cmds, t_env **env, t_ms_state *status)
+{
+	int		num_cmds;
+	t_cmd	*current;
+	int		i;
+	int		pid;
+	int		exit_status;
+	int		**pipe_ends;
+	int		j;
+	int	stat;
+
+	num_cmds = cmds_len((*cmds));
+	
 
 	current = *cmds;
 	i = 0;
+	// if (!ft_strcmp(current->cmd_arr[0], "exit"))
+	// {
+	// 	return (execute_builtins(cmds, (*cmds)->cmd_arr, env, status));
+	// }
+	pipe_ends = init_pipe_ends(num_cmds);
+	init_pipes(pipe_ends, num_cmds);
 	while (current)
 	{
+		//if encounter any of the builtin unallowed, exit out of the process
+		//so dont need to check here and execute anymore
+		if (current->builtin && (!ft_strcmp(current->cmd_arr[0], "cd")
+                || !ft_strcmp(current->cmd_arr[0], "export")
+                || !ft_strcmp(current->cmd_arr[0], "unset")))
+            //    || !ft_strcmp(current->cmd_arr[0], "exit")))
+        {
+            // Execute built-in commands directly in parent process
+            execute_builtins(&current, current->cmd_arr, env, status);
+            current = current->next;
+            continue;
+        }
+		else if (!ft_strcmp(current->cmd_arr[0], "exit"))
+		{
+			i = 0;
+			while (i++ < num_cmds - 1)
+				free(pipe_ends[i]);
+			free(pipe_ends);
+			return (execute_builtins(cmds, (*cmds)->cmd_arr, env, status));
+		}
 		pid = fork();
 		if (pid < 0)
 		{
@@ -205,20 +200,18 @@ void	execute_pipeline(t_cmd **cmds, t_env **env, t_ms_state *status)
 				j++;
 			}
 			do_redirection(current, status);
+			//check if builtin is unallowed, exit out of child process
+			//only execute builtins if its allowed
 			if (current->builtin)
-			{
 				execute_builtins(&current, current->cmd_arr, env, status);
-				// stat = status->exit_status;
-				// free_all_and_exit(cmds, env, status);
-				exit(status->exit_status);
-			}
 			else
-			{
 				execute_cmd(current, env, status);
-				// stat = status->exit_status;
-				// free_all_and_exit(cmds, env, status);
-				exit(status->exit_status);
-			}
+			stat = status->exit_status;
+			free_all_and_exit(cmds, env, status);
+			while (i++ < num_cmds - 1)
+				free(pipe_ends[i]);
+			free(pipe_ends);
+			exit(stat);
 		}
 		current = current->next;
 		i++;
@@ -233,6 +226,7 @@ void	execute_pipeline(t_cmd **cmds, t_env **env, t_ms_state *status)
 	}
 
 	i = 0;
+	exit_status = 0;
 	while (i < num_cmds)
 	{
 		wait(&exit_status);
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ting <ting@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 13:58:54 by ting              #+#    #+#             */
-/*   Updated: 2024/06/21 12:39:17 by ting             ###   ########.fr       */
+/*   Updated: 2024/06/21 17:12:28 by ting             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,7 @@ int	main(int ac, char **av, char **env)
 				continue;
 			}
 			free(line);
+        //call single cmd if only one cmd
         //    do_single_cmd(cmds, env_dup, status);
             execute_pipeline(cmds, env_dup, status);
 			free_cmds(cmds);
